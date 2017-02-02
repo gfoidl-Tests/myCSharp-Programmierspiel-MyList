1f6d67f59f24d0f10e629f879aad9c3252b2a047
Günther Foidl <gue@korporal.at>
2017-02-02 18:02:53 +0100

    Tests für IndexOf erweitert

--- Inflames2K.Tests/MyListTests/IndexOf.cs
+++ Inflames2K.Tests/MyListTests/IndexOf.cs
@@ -30,6 +30,40 @@ namespace Inflames2K.Tests.MyListTests
 		}
 		//---------------------------------------------------------------------
 		[Test]
+		public void Item_is_contained_in_lower_half___returns_index()
+		{
+			var sut = new MyList<int>();
+			sut.Add(2);
+			sut.Add(1);
+			sut.Add(3);
+			sut.Add(4);
+			sut.Add(5);
+
+			Assume.That(sut.Count, Is.EqualTo(5));
+
+			int actual = sut.IndexOf(1);
+
+			Assert.AreEqual(1, actual);
+		}
+		//---------------------------------------------------------------------
+		[Test]
+		public void Item_is_contained_in_upper_half___returns_index()
+		{
+			var sut = new MyList<int>();
+			sut.Add(2);
+			sut.Add(1);
+			sut.Add(3);
+			sut.Add(4);
+			sut.Add(5);
+
+			Assume.That(sut.Count, Is.EqualTo(5));
+
+			int actual = sut.IndexOf(4);
+
+			Assert.AreEqual(3, actual);
+		}
+		//---------------------------------------------------------------------
+		[Test]
 		public void Item_is_not_contained___returns_minus_1([Values(0, -1, int.MinValue, int.MaxValue, 42)]int value)
 		{
 			var sut = new MyList<int>();
